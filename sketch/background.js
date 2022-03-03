17964cd3ad7db9ad8051e9cf8d3e689758d427aa
sammylevin10 <sammylevin10@gmail.com>
2022-03-03 03:03:44 -0500

    Main

---
+++ sketch/background.js
@@ -0,0 +1,27 @@
+
+// function setup() {
+//   var canvasDiv = document.getElementById('background-container');
+//   var divWidth = canvasDiv.offsetWidth;
+//   let cnv = createCanvas(divWidth, windowHeight);
+//   cnv.parent("#background-container");
+// }
+
+// function draw() {
+    // // background(40, 40, 40);
+    // background(255, 0, 0);
+    // // noFill();
+    // // strokeWeight(2);
+  
+    // // for (let i = 0; i < ranges; i++) {
+    // //   let paint = map(i, 0, ranges, 40, 50);
+    // //   stroke(paint);
+      
+    // //   beginShape();
+    // //   for (let x = -10; x < width + 11; x += 20) {
+    // //     let n = noise(x * 0.001, i * 0.01, frameCount * 0.005);
+    // //     let y = map(n, 0, 1, 0, height);
+    // //     vertex(x, y);
+    // //   }
+    // //   endShape();
+    // // }
+//   }
